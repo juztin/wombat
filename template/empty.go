e410b2975aadfb0dd0eb590a30042cc4e1bf6bc4
Justin Wilson <justinwilson1@gmail.com>
2013-10-25 14:50:53 -0600

    license header

--- template/empty.go
+++ template/empty.go
@@ -1,3 +1,7 @@
+// Copyright 2013 Justin Wilson. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package template
 
 const Empty = "{{define \"title\"}}Template Doesn't Exist{{end}}\n" +
